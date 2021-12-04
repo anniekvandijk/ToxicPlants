9f4cf13271abc5ecf11dc6666558abc1a212a96c
anniekvandijk <anniek@animundo.nl>
2021-12-04 09:50:55 +0100

    changed plantService and plantrequest

--- Function/Services/FakePlantRequest.cs
+++ Function/Services/FakePlantRequest.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -40,6 +42,25 @@ namespace Function.Services
                     , "Utilities", fileName);
 
             return await File.ReadAllTextAsync(path);
+        }
+
+        public async Task<HttpResponseMessage> MakeRequest(HttpRequestMessage httpRequestMessage)
+        {
+            _logger.LogInformation("Fake Plant Service called");
+
+            var fileName = Environment.GetEnvironmentVariable("FAKE_PLANTREQUEST_FILENAME");
+
+            var path =
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ??
+                             throw new InvalidOperationException("File not found")
+                    , "Utilities", fileName);
+            
+            var content =  await File.ReadAllTextAsync(path);
+
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+            responseMessage.Content = new StringContent(content);
+            return responseMessage;
+
         }
     }
 }
