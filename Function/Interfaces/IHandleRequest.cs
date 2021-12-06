e4514cd8b5d619a4f67fad3af21b66479a37f858
Anniek van Dijk <18325609+anniekvandijk@users.noreply.github.com>
2021-12-06 23:59:30 +0100

    Update IHandleRequest.cs

--- Function/Interfaces/IHandleRequest.cs
+++ Function/Interfaces/IHandleRequest.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
+using System.IO;
 using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Function.Interfaces
 {
     internal interface IHandleRequest
     {
-        Task CollectData(HttpRequestData request);
+        Task CollectData(Stream requestBody);
     }
 }
